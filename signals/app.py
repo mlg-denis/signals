62fb88fa5686a6f6c7203ca544bcb0a2fd281171
mlg-denis <mlg-denis@outlook.com>
2025-11-15 14:45:45 +0000

    Add moving averages and crossovers to web app Handle None values Tidy plotter.py

--- signals/app.py
+++ signals/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from financeinfo import fetch
 from backtesting import run_backtest
 from plotter import get_fig
+import indicators as indct
 
 def main():
     tickers = ["AAPL", "NVDA", "TSLA"]
@@ -10,8 +11,10 @@ def main():
                         index=None)
 
     if ticker:
-        data = fetch(ticker)
-        fig = get_fig(data,ticker)
+        data = fetch(ticker, period = "1y")
+        ema12 = indct.compute_ema(data["Close"],12)
+        ema26 = indct.compute_ema(data["Close"],26)
+        fig = get_fig(data,ticker, ema12, "EMA12", ema26, "EMA26", True)
         st.pyplot(fig)
 
 if __name__ == "__main__":
--- signals/plotter.py
+++ signals/plotter.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from indicators import detect_crossovers
 
 signal_size = 100
 
@@ -27,17 +28,48 @@ def plot_crossovers(data: pd.DataFrame, column: str):
         marker = "v", color = "red", s = signal_size, label = "Bearish Crossover"
     )
 
-def get_fig(data, ticker):
+def get_fig(data: pd.DataFrame, ticker: str,
+            short_avg: pd.Series = None, short_label: str = None,
+            long_avg: pd.Series = None, long_label: str = None,
+            plot_crossovers: bool = False):
+    
     fig, ax = plt.subplots(figsize = (10,5))
-    ax.plot(data["Close"])
+
+    ax.plot(data["Close"], label = "Price")
+
+    if not short_avg.empty:
+        assert short_label, "You must supply a label when providing short_avg."
+        ax.plot(short_avg, label = short_label, linestyle = "--")
+
+    if not long_avg.empty:
+        assert long_label, "You must supply a label when providing long_avg."
+        ax.plot(long_avg, label = long_label, linestyle = "--")
+
+    if plot_crossovers:
+        
+        assert not short_avg.empty, "You must supply a short average to plot crossover points."
+        assert not long_avg.empty, "You must supply a long average to plot crossover points."
+        
+        try:
+            data["Crossover"] = detect_crossovers(short_avg, long_avg)
+            size = 7.5
+
+            ax.plot(
+                data.index[data["Crossover"] == 1],
+                data["Close"][data["Crossover"] == 1],
+                marker="^", color="green", linestyle="none", markersize = size, label="Bullish Crossover"
+            )
+
+            ax.plot(
+                data.index[data["Crossover"] == -1],
+                data["Close"][data["Crossover"] == -1],
+                marker="v", color="red", linestyle="none", markersize = size, label="Bearish Crossover"
+            )
+        except Exception as e:
+            print(f"Attempt to plot crossover failed: {e}")
+
     ax.set_title(ticker)
     ax.set_xlabel("Date")
     ax.set_ylabel("Price, USD")
-    return fig
-
-def title(title):
-    plt.title(title)
-
-def show():
-    plt.legend()
-    plt.show()    
+    ax.legend()
+    return fig  
