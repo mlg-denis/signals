d6de4f5f93503ac6e2eb8c903fa6f7f3f900a24b
mlg-denis <mlg-denis@outlook.com>
2025-11-15 12:20:12 +0000

    Rename data.py to financeinfo.py

--- signals/main.py
+++ signals/main.py
@@ -1,30 +1,30 @@
-import data
-import indicators
-import plot
+import financeinfo
+import indicators as indc
+import plot as p
 import backtesting
 
 def main():
     ticker = "TSLA"
-    info = data.fetch(ticker)
+    info = financeinfo.fetch(ticker)
     closes = info["Close"]
 
-    sma20 = indicators.compute_sma(closes, 20)
-    sma50 = indicators.compute_sma(closes, 50)
-    ema12 = indicators.compute_ema(closes, 12)
-    ema26 = indicators.compute_ema(closes, 26)
+    sma20 = indc.compute_sma(closes, 20)
+    sma50 = indc.compute_sma(closes, 50)
+    ema12 = indc.compute_ema(closes, 12)
+    ema26 = indc.compute_ema(closes, 26)
    
     column = "Crossover"
-    info[column] = indicators.detect_crossovers(sma20, sma50)
+    info[column] = indc.detect_crossovers(sma20, sma50)
 
     backtesting.run_backtest(info, column)
 
-    plot.init()
-    plot.plot_series(closes, "Price", "-", 0.7)
-    plot.plot_series(ema12, "EMA12", "--", 0.35)
-    plot.plot_series(ema26, "EMA26", "--", 0.35)
-    plot.plot_crossovers(info, column)
-    plot.title(ticker)
-    plot.show()
+    p.init()
+    p.plot_series(closes, "Price", "-", 0.7)
+    p.plot_series(ema12, "EMA12", "--", 0.35)
+    p.plot_series(ema26, "EMA26", "--", 0.35)
+    p.plot_crossovers(info, column)
+    p.title(ticker)
+    p.show()
 
 if __name__ == "__main__":
     main()
